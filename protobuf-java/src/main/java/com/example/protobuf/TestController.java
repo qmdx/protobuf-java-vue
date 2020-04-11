29bb6e766e2b8d39dca16693dc97fbbc18da664d
qmdx <243194995@qq.com>
2020-04-11 09:56:08 +0800

    Update TestController.java

--- protobuf-java/src/main/java/com/example/protobuf/TestController.java
+++ protobuf-java/src/main/java/com/example/protobuf/TestController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Google Protobuf 测试
@@ -14,13 +18,14 @@ import java.io.IOException;
  */
 @RestController
 public class TestController {
+    private long LENGTH = 2000000;
 
     @CrossOrigin
     @GetMapping("/pb")
     public void pb(HttpServletResponse response) {
         try {
             WindGps.Wind.Builder builder = WindGps.Wind.getDefaultInstance().toBuilder();
-            for (int i=0; i< 200000; i++) {
+            for (int i=0; i< LENGTH; i++) {
                 WindGps.Wind.Gps.Builder wgb = WindGps.Wind.Gps.getDefaultInstance().toBuilder();
                 wgb.setLongitude(30.3);
                 wgb.setDimension(130.11);
@@ -31,5 +36,20 @@ public class TestController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    @CrossOrigin
+    @GetMapping("/json")
+    public List<Map<String, Double>> json(HttpServletResponse response) {
+        List<Map<String, Double>> wind = new ArrayList<>();
+        for (int i=0; i< LENGTH; i++) {
+            Map<String, Double> gps = new HashMap<>();
+            gps.put("l", 30.3);
+            gps.put("d", 130.11);
+            gps.put("c", 173.5);
+            wind.add(gps);
+        }
+        return wind;
     }
 }
